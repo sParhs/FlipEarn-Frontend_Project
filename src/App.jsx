4786634b23e92903c8ff3ff0eca67bdf6d041aba
sParhs <sparshs752@gmail.com>
2026-01-15 23:52:29 +0530

    Frontend project commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import {Route, Routes,useLocation} from 'react-router-dom'
+import Home from './pages/Home'
+import Marketplace from './pages/Marketplace'
+import MyListings from './pages/MyListings'
+import ListingDetails from './pages/ListingDetails'
+import ManageListing from './pages/ManageListing'
+import Messages from './pages/Messages'
+import MyOrders from './pages/MyOrders'
+import Loading from './pages/Loading'
+import Navbar from './components/navbar'
+import ChatBox from './components/ChatBox'
+import {Toaster} from 'react-hot-toast'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AllListings from './pages/admin/AllListings'
+import CredentialChange from './pages/admin/CredentialChange'
+import CredentialVerify from './pages/admin/CredentialVerify'
+import Transactions from './pages/admin/Transactions'
+import Withdrawal from './pages/admin/Withdrawal'
+
+const App = () => {
+  const {pathname} = useLocation()
+  return (
+    <div>
+    <Toaster />
+    {!pathname.includes('/admin') && <Navbar />}
+     <Routes>
+       <Route path='/' element={<Home />} />
+       <Route path='/Marketplace' element={<Marketplace />} />
+       <Route path='/my-listings' element={<MyListings />} />
+       <Route path='/listing/:listingId' element={<ListingDetails />} />
+       <Route path='/create-listing' element={<ManageListing />} />
+       <Route path='/edit-listing/:id' element={<ManageListing />} />
+       <Route path='/messages' element={<Messages />} />
+       <Route path='/my-orders' element={<MyOrders />} />
+       <Route path='/loading' element={<Loading />} />
+       <Route path='/admin' element={<Layout />}>
+        <Route index element={<Dashboard />}/>
+        <Route path='verify-credentials' element={<CredentialVerify />}/>
+        <Route path='change-credentials' element={<CredentialChange />}/>
+        <Route path='list-listings' element={<AllListings />}/>
+        <Route path='transactions' element={<Transactions />}/>
+        <Route path='withdrawal' element={<Withdrawal />}/>
+       </Route>
+     </Routes>
+    <ChatBox />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Marketplace.jsx
@@ -0,0 +1,87 @@
+import { ArrowLeftIcon, FilterIcon } from 'lucide-react'
+import React, { useState } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import ListingCard from '../components/ListingCard'
+import FilterSidebar from '../components/FilterSidebar'
+
+const Marketplace = () => {
+
+  const [searchParams] = useSearchParams()
+  const search = searchParams.get("search")
+
+  const navigate = useNavigate()
+  const [showFilterPhone, setShowFilterPhone] = useState(false)
+  const [filters, setFilters] = useState({
+     platform: null,
+     maxPrice: 100000,
+     minFollowers: 0,
+     niche: null,
+     verified: false,
+     monetized: false,
+
+  })
+  const {listings} = useSelector(state => state.listing)
+  const filteredListings = listings.filter((listing)=>{
+    if(filters.platform && filters.platform.length > 0){
+      if(!filters.platform.includes(listing.platform)) return false
+    }
+    
+    if(filters.maxPrice){
+      if(!filters.price > filters.maxPrice) return false
+    }
+
+    if(filters.minFollowers){
+      if(!filters.followers_count < filters.minFollowers) return false
+    }
+
+    if(filters.niche && filters.niche.length > 0){
+      if(!filters.niche.includes(listing.niche)) return false
+    }
+
+    if(filters.verified && listing.verified!== filter.verified) return false
+
+    if(filters.monetized && listing.verified!== filter.monetized) return false
+    
+    if(search){
+      const trimed = search.trim();
+      if(
+        !listing.title.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.username.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.description.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.platform.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.niche.toLowerCase().includes(trimed.toLowerCase())
+      )
+      return false
+    }
+
+    return true
+  })
+
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
+
+        <div className='flex items-center justify-between text-slate-500'>
+        <button onClick={()=>{navigate(`/`); scrollTo(0,0)}} className='flex items-center gap-2 py-5'>
+        <ArrowLeftIcon className='size-4' />
+        Back to Home
+        </button>
+        <button onClick={()=>setShowFilterPhone(true)} className='flex sm:hidden items-center gap-2 py-5'> 
+        <FilterIcon className='size-4'/>
+        Filters
+        </button>
+        </div>
+
+        <div className=' relative flex items-start justify-between gap-8 pb-8'>
+          <FilterSidebar setFilters={setFilters} filters={filters} setShowFilterPhone={setShowFilterPhone} showFilterPhone={showFilterPhone}/>
+          <div className='flex-1 grid xl:grid-cols-2 gap-4'>
+            {filteredListings.sort((a,b)=>a.featured ? -1 : b.featured ? 1 : 0).map((listing, index)=>(
+              <ListingCard listing={listing} key={index}/>
+            ))}
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Marketplace
